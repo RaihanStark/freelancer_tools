61822268e1cbee9b98156835d8a42ea10cc6bdfd
Raihan Yudo Saputra <raihanyudosaputra30@gmail.com>
2020-06-21 00:21:33 +0700

    add kurs converter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Layout from "./layout/Layout";
 import Homepage from "./containers/homepage/Homepage";
+import KursConverter from "./containers/KursConverter/KursConverter";
+
 function App() {
   return (
     <Layout>
-      <Homepage />
+      <KursConverter />
     </Layout>
   );
 }
---
+++ src/containers/KursConverter/KursConverter.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Container, Paper, Typography } from "@material-ui/core";
+class KursConverter extends Component {
+  render() {
+    return (
+      <Container>
+        <Paper>
+          <Typography>Kurs Calculator</Typography>
+        </Paper>
+      </Container>
+    );
+  }
+}
+
+export default KursConverter;
