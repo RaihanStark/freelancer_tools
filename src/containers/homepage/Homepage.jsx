f350cc74dc3c64336aeb763fac3a6c0da594f4ec
Raihan Yudo Saputra <raihanyudosaputra30@gmail.com>
2020-06-21 00:15:30 +0700

    add homepage

--- src/containers/homepage/Homepage.jsx
+++ src/containers/homepage/Homepage.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-import { Container } from "@material-ui/core";
+import { Container, Typography } from "@material-ui/core";
 import CardList from "../../UI/CardList/CardList";
+import Grid from "@material-ui/core/Grid";
+
 const Homepage = () => {
   return (
     <Container>
+      <Typography variant="h6">List of Tools:</Typography>
+
       <CardList title="Kurs Calculator" />
     </Container>
   );
